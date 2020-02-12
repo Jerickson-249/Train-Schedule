1ea51efcec6ebb93e12f0f915473a767c74a1e5f
Jacob Erickson <jakedasnake6_12@hotmail.com>
2020-02-11 21:39:32 -0700

    Updating the htmal and javascript

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var firebaseConfig = {
     messagingSenderId: "598298892240",
     appId: "1:598298892240:web:9b302d7e7f14bf8b080bd5"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var db = firebase.database()
@@ -16,27 +17,52 @@ var db = firebase.database()
 
 $("#addtrain").on("click", function () {
     event.preventDefault()
+
+    //Stored and retrieved new train data information
     var trainName = $("#trainname").val()
     var destination = $("#Destination").val()
     var firsttraintime = $("#First-Train-Time").val()
     var frequency = $("#Frequency").val()
+
+    //Employee Information
     console.log(trainName, destination, firsttraintime, frequency)
+
     // push the train to the database
     db.ref().push({
         trainName: trainName,
         destination: destination,
         firstTrain: firsttraintime,
-        frequency: frequency
-
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
     })
-
+    $("form")[0].reset();
 })
 
-
 // show the info after calculate next train and minutes away
+//Creating a firebase event for adding the data from the new trains and then putting them in the DOM
+db.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val().trainName)
 
-db.ref().on("child_added", function(data){
-    console.log(data.val().trainName)
+// calculate next train and minutes away 
+var nextArrival;
+var minsAway;
+
+//Change year so that the first train comes before the current time
+var firsttrainNew = moment(childsnapshot.val().firstTrain,"hh:mm").subtract(1,"years");
+
+//Stating the difference between the firstTrain and the current train
+var diffTime = moment().diff(moment(firstTrainNew),"minutes");
+var remainder = diffTime % childsnapshot.val().frequency;
+
+//Minutes away from the next train
+var minsAway = childsnapshot.val().frequency - remainder;
+
+//Next train time
+var nextTrain = moment(nextTrain).format("hh:mm");
+
+// you know the firsttrain, frequency  // current time
+
+$("#trains").append(trow)
 
 var trow= `<tr>
 
@@ -46,7 +72,18 @@ var trow= `<tr>
 <td>${data.val().frequency}</td>
 </tr>`
 
-$("#trains").append(trow)
+//Appending the added row to the table provided
 
+//add the data into the DOM/ HTML
+$("#add-row").append("<tr><td>" + childsnapshot.val().name +
+    "</td><td>" + childSnapshot.val().destination +
+    "</td><td>" + childSnapshot.val().frequency +
+    "</td><td>" + nextTrain + 
+    "</td><td>" + minsAway + "</td></tr>");
 
-})
+//Deal with the errors
+}, function(errorObject) {
+    console.log("Errors Handled: " + errorObject.code);
+
+});
+
